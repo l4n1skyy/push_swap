a502f0e0fc69f4c2f1d4dea6315aefadc62708fc
Lani <lanishaanusri28@gmail.com>
2026-09-10 18:27:45 +0800

    Update push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -21,7 +21,4 @@ t_node	*ft_llstnew(int number);
 void	ft_llstaddback(t_node **head, t_node *node);
 void	ft_llstclear(t_node **head);
 
-/* other utils */
-t_node *handle_fail(t_node **head);
-
 #endif
