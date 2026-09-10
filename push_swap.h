7b1665f57a133516dd1fd95811560dac7b472fe4
Lanisha <lanishaanusri28@gmail.com>
2026-09-10 18:20:29 +0800

    input validation

--- push_swap.h
+++ push_swap.h
@@ -13,8 +13,15 @@ typedef struct s_node
 	struct s_node	*prev;
 }	t_node;
 
+/* main functions */
+t_node *create_struct(char **argv);
+
 /* doubly linked list functions */
 t_node	*ft_llstnew(int number);
 void	ft_llstaddback(t_node **head, t_node *node);
+void	ft_llstclear(t_node **head);
+
+/* other utils */
+t_node *handle_fail(t_node **head);
 
 #endif
